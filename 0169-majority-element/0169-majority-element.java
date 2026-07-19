04da2d78b37e89f0ce2a6eba348bd2472e3c1c2a
Malcolm Mwawongo <111082289+Malcolm254@users.noreply.github.com>
2026-07-19 15:41:39 +0300

    Time: 1 ms (99.9%), Space: 55.6 MB (59.95%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+    int count = 0;
+    int candidate = 0;
+
+    for (int num : nums) {
+        if (count == 0) {
+            candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+
+    return candidate;
+}
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
