93ffd08a672e880895244cb5f1291de327cdfb99
Malcolm Mwawongo <111082289+Malcolm254@users.noreply.github.com>
2026-07-20 18:08:21 +0300

    Time: 7 ms (73.99%), Space: 49.9 MB (26.58%) - LeetHub - Time: O(N * K log K), Space: O(N * K)

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,18 @@
+class Solution {
+ public List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, List<String>> groups = new HashMap<>();
+
+    for (String s : strs) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        String key = new String(chars);
+        groups.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+    }
+
+    return new ArrayList<>(groups.values());
+  }  
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
