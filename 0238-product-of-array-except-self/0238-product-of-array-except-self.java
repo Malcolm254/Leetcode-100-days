0c6d6bdc639147657ef7a43f567586a2fc5685f9
Malcolm Mwawongo <111082289+Malcolm254@users.noreply.github.com>
2026-07-22 21:09:59 +0300

    Time: 2 ms (93.98%), Space: 72.3 MB (6.41%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+    int n = nums.length;
+    int[] result = new int[n];
+
+    int prefix = 1;
+    for (int i = 0; i < n; i++) {
+        result[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    int suffix = 1;
+    for (int i = n - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
+}
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
